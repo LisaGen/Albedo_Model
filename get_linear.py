47b7f600c01b692942c068c40ccb8a1b7d0ba30b
LisaGen <93012058+LisaGen@users.noreply.github.com>
2023-09-07 10:29:29 +0200

    Update get_albedo_function.py

--- get_linear.py
+++ get_linear.py
@@ -9,27 +9,20 @@ import glob
 import matplotlib.pyplot as plt
 
 
-# Read data from CSV file
-#csv_file_path = '/localdata/lgeneros/albedo_function.csv'
-#data = pd.read_csv(csv_file_path)
-
-# Specify the columns for the scatter plot
-#x_column = "BCID"
-#y_column = "Albedo_correction"
+#Plot the first 20 terms of the albedo array as a function of the BCID both for the old and the updated version of the model
+#Performs an interpolation to estimate the fourth entry of the array, in order to reprodce an exponential decrease
 
 
-#data = data[["BCID", "Albedo_correction"]]
-#x = data[x_column]
 albedo_model_old = np.array([1.0, 0.022410375592529844, 0.0023920368925298427, 0.0007854522325298428])
 albedo_tail = [0.00030464839252984286, 0.00022096663252984283, 0.00018857369252984284, 0.00017816717252984283, 0.00017519799252984283, 0.00017183887252984284, 0.00016958514252984284, 0.00016651578252984285, 0.00016273452252984284, 0.00016219435252984283, 0.00015808399252984284, 0.00015572652252984285, 0.00015465688252984283, 0.00015297554252984285, 0.00015145875252984284, 0.00014926227252984284]
 
-print(len(albedo_tail))
 
-x = np.arange(1, 21)
+x = np.arange(1, 21) 
 y = np.array([1.0])
 y_old = np.concatenate((albedo_model_old, albedo_tail)) *100
 print(len(y_old))
 
+#Estimate of new Alebdo terms from average over a set of Run-3 fills
 ave_1 = 0.005244808716170002
 ave_2 = 0.0007509161683906839
 ave_3 = 4.3792521392825125e-05
@@ -43,7 +36,7 @@ y = np.append(y,ave_3)
 
 y = np.append(y,albedo_tail)
 
-y = y*100
+y = y*100 #transofrmed in percentage (logarithmic scale does not allow the plotting of values smaller than 0.01)
 
 
 # Calculate the slope and intercept
@@ -65,9 +58,9 @@ plt.yscale('log')
 
 plt.scatter(x+0.2, y_old, color='dodgerblue', label='Old Albedo')
 plt.scatter(x, y, color='yellowgreen', label='New Albedo')
-#plt.plot(x_line_fit, y_line_fit, color='orange', label='Interpolation Line')
+plt.plot(x_line_fit, y_line_fit, color='orange', label='Interpolation Line')
 plt.scatter(x[3], third_correction, color='yellowgreen', marker='o')
-#plt.text(12,7,'Fitted third correction: '+str(third_correction_tr)+'%', fontsize=8, color='black', ha='left', va='center', weight='bold')
+plt.text(12,7,'Fitted third correction: '+str(third_correction_tr)+'%', fontsize=8, color='black', ha='left', va='center', weight='bold')
 
 
 plt.xticks(np.arange(1, 21),np.arange(1, 21))
