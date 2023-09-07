bee08dfc616cc7d35b0bc70f7de8e30b851f6d82
LisaGen <93012058+LisaGen@users.noreply.github.com>
2023-09-07 10:17:27 +0200

    Update get_average.py

--- get_average.py
+++ get_average.py
@@ -1,12 +1,14 @@
 import csv
+#Provides the average value of a column in a csv file
+#Used to estimate correction terms for the update of the Albedo model
 
 # Read data from CSV file
-csv_file_path = '/localdata/lgeneros/data_new_albedo_3eb.csv'
+csv_file_path = '/localdata/lgeneros/data_new_albedo_3eb.csv' #change with your file path
 data = []
 
 with open(csv_file_path, mode='r') as file:
     reader = csv.reader(file)
-    next(reader)
+    next(reader) #skip the frist row of coments
     for row in reader:
         data.append(float(row[3]))  # Assuming the value is in the second column
 
